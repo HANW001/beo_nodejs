6974127614f9f0c8269169fd33f85c65f813aa96
HANW001 <mosadi@nate.com>
2021-10-26 14:08:46 +0900

    add

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const static = require('serve-static');
+// const router = require("./routes/user"); //라우터 모듈 등록 (라우터 모듈안에 다이어리 스키마 모듈을 불러오고 있으므로 아래와 같이 라우터만!
+// const punchListRouter = require("./routes/punchList"); 
+// const summuryRouter = require("./routes/summury"); 
+// let sequelize = require('./models/index').sequelize;
+let app = express();
+// sequelize.sync();
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(
+    session({
+        key: "loginData",
+        secret: "testSecret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24
+        }
+    })
+);
+app
+    // .use(express.static(path.join(__dirname, 'upload')))
+    .use(static(path.join(__dirname, 'upload')))
+    // .use('/api/', router)
+    // .use('/punchlist/', punchListRouter)
+    // .use('/summury/', summuryRouter)
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    // .get('/', (req, res) => res.render('pages/index'))
+    .get('/', handleHome)
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+console.log('hello world');
+
+function handleHome(req, res){
+    res.send('hello ');
+}
---
+++ models/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config, {
+        host: "localhost",
+        dialect: "mysql",
+        operatorAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config, {
+        host: "localhost",
+        dialect: "mysql",
+        operatorAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+}
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.Area = require('./area')(sequelize, Sequelize);
+db.Authority = require('./authority')(sequelize, Sequelize);
+db.Category = require('./category')(sequelize, Sequelize);
+db.Department = require('./department')(sequelize, Sequelize);
+db.Discipline = require('./discipline')(sequelize, Sequelize);
+db.Drawing = require('./drawing')(sequelize, Sequelize);
+db.Photos = require('./photos')(sequelize, Sequelize);
+db.Progress = require('./progress')(sequelize, Sequelize);
+db.Project = require('./project')(sequelize, Sequelize);
+db.ProjectUser = require('./projectUser')(sequelize, Sequelize);
+db.PunchList = require('./punchList')(sequelize, Sequelize);
+db.PunchLoc = require('./punchLoc')(sequelize, Sequelize);
+db.ScheduleDate = require('./scheduleDate')(sequelize, Sequelize);
+db.Stage = require('./stage')(sequelize, Sequelize);
+db.Status = require('./status')(sequelize, Sequelize);
+db.Subsystem = require('./subsystem')(sequelize, Sequelize);
+db.System = require('./system')(sequelize, Sequelize);
+db.TagNumberDetail = require('./tagNumberDetail')(sequelize, Sequelize);
+db.Unit = require('./unit')(sequelize, Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
+db.UserComment = require('./userComment')(sequelize, Sequelize);
+db.PunchListLog = require('./punchListLog')(sequelize, Sequelize);
+db.VwPunchHis = require('./vwPunchHis')(sequelize, Sequelize);
+
+module.exports = db;
