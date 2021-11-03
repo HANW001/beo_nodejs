7bfa8ed490110335a0afd6a62252411799dd601f
HANW001 <mosadi@nate.com>
2021-11-03 20:29:25 +0900

    edit admin page

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ const cookieParser = require('cookie-parser');
 const static = require('serve-static');
 const router = require("./routes/user"); //라우터 모듈 등록 (라우터 모듈안에 다이어리 스키마 모듈을 불러오고 있으므로 아래와 같이 라우터만!
 const giftRouter = require("./routes/gift"); 
+const fs = require('fs');
+const ejs = require('ejs');
 
 const passport = require('passport')
 const KakaoStrategy = require('passport-kakao').Strategy
 // const summuryRouter = require("./routes/summury"); 
 let sequelize = require('./models/index').sequelize;
 let app = express();
+
+const mainPage = fs.readFileSync('views/pages/admin.ejs', 'utf8');
+ 
+app.get('/', (req, res) => {
+    var page = ejs.render(mainPage, {
+        title: "Temporary Title",
+    });
+    res.send(page);
+});
 sequelize.sync();
 
 app.use(cors({
@@ -39,7 +50,7 @@ app
     // .use('/summury/', summuryRouter)
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
+    // .get('/', (req, res) => res.render('pages/admin'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
@@ -82,6 +93,7 @@ app.get('/oauth', passport.authenticate('kakao'), function (req, res) {
     // res.send(`nickname : ${req.user.profile.properties.nickname} nickname : ${req.user.profile.properties.profile_image}`)
     res.json({login: true, nickname:req.user.profile.properties.nickname, img:req.user.profile.properties.profile_image})
 })
+
 
 
 
--- routes/gift.js
+++ routes/gift.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const fs = require('fs');
+const ejs = require('ejs');
 
 const { users } = require('../models');
 let router = express.Router();
+
+const mainPage = fs.readFileSync('views/pages/admin.ejs', 'utf8');
+const listPage = fs.readFileSync('views/pages/list.ejs', 'utf8');
 
 router.get('/test', (req, res) => {
     users.findAll({
@@ -9,7 +14,42 @@ router.get('/test', (req, res) => {
     })
     .then(result => {
         // res.json({"data":result, test: "test", error: null})
-        res.json(result)
+        var page = ejs.render(mainPage, {
+            title: "Temporary Title",
+            data:result
+        });
+        res.send(page)
+        // res.json(result)
+    })
+    .catch(err => {
+        console.error(err);
+        res.json({error: null}
+    )});
+})
+
+router.get('/test2', (req, res) => {
+    var page = ejs.render(listPage, {
+        title: "Temporary Title",
+    });
+    res.send(page);
+});
+
+router.get('/test3', (req, res) =>  {
+    var id = req.query.id;
+    // var id = req.body.id;
+    users.findAll({
+        attributes: ['id', 'password'],
+        where: {id: id}
+    })
+    .then(result => {
+        // res.json({"data":result, test: "test", error: null})
+        var page = ejs.render(listPage, {
+            title: "Temporary Title",
+            data:result
+        });
+        // console.log(data)
+        res.send(page)
+        // res.json(result)
     })
     .catch(err => {
         console.error(err);
